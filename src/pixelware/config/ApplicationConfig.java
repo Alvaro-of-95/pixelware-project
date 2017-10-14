d083fbbfa48d73d50c45a76a92f77ff9aff9c711
Alvaro-of-95 <32034476+Alvaro-of-95@users.noreply.github.com>
2017-10-14 20:31:10 +0200

    Update ApplicationConfig.java

--- src/pixelware/config/ApplicationConfig.java
+++ src/pixelware/config/ApplicationConfig.java
@@ -13,20 +13,20 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 excludeFilters={@Filter(type=FilterType.ANNOTATION, value=Configuration.class)})
 public class ApplicationConfig {
 	
-	/* Método que devuelve un bean DataSource
-	 * con la conexión a la BBDD: */
+	/* MÃ©todo que devuelve un bean DataSource
+	 * con la conexiÃ³n a la BBDD: */
 	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource managerDataSource = new DriverManagerDataSource();
 		managerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		managerDataSource.setUrl("jdbc:mysql://sql11.freemysqlhosting.net:3306/sql11198254");
-		managerDataSource.setUsername("sql11198254");
-		managerDataSource.setPassword("fM4bE8BVdf");
+		managerDataSource.setUrl("jdbc:mysql://sql11.freemysqlhosting.net:3306/sql11199452");
+		managerDataSource.setUsername("sql11199452");
+		managerDataSource.setPassword("Xx7mps271q");
 		
 		return managerDataSource;
 	}
 	
-	/* Método que devuelve un bean JdbcTemplate asociado al
+	/* MÃ©todo que devuelve un bean JdbcTemplate asociado al
 	 * DataSource para trabajar directamente con la BBDD: */
 	@Bean
 	public JdbcTemplate jdbcTemplate(DataSource dataSource) {
